894fd4bea19132cb4dffc8f1e8b4041403d614bf
InahoAZ <santyyvii@gmail.com>
2016-10-22 19:40:21 -0300

    BD actual

--- proyecto.sql
+++ proyecto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-10-2016 a las 17:24:29
+-- Tiempo de generación: 22-10-2016 a las 22:39:49
 -- Versión del servidor: 5.7.14
 -- Versión de PHP: 5.6.25
 
@@ -23,30 +23,39 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `estudiantes`
+-- Estructura de tabla para la tabla `estudiante`
 --
 
-CREATE TABLE `estudiantes` (
+CREATE TABLE `estudiante` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(200) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
   `edad` int(11) NOT NULL,
-  `promedio` double NOT NULL,
-  `imagen` varchar(200) NOT NULL,
-  `fecha` date NOT NULL,
-  `id_seccion` int(11) NOT NULL
+  `curso` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Volcado de datos para la tabla `estudiantes`
+-- Volcado de datos para la tabla `estudiante`
 --
 
-INSERT INTO `estudiantes` (`id`, `nombre`, `edad`, `promedio`, `imagen`, `fecha`, `id_seccion`) VALUES
-(8, 'Roberto Musso', 41, 10, '1916musso.JPG', '2016-07-26', 1),
-(9, 'Santiago Tavella', 39, 5, '2010tavella.jpg', '2016-07-26', 1),
-(10, 'Matt Bellamy', 39, 10, '2043bellamy.jpg', '2016-07-26', 1),
-(11, 'Dominic Howard', 35, 8, '2109dominic.jpg', '2016-07-26', 1),
-(5, 'Tu vieja', 15, 10, '1046de nada.jpg', '2016-07-24', 1),
-(6, 'elwn', 8, 8, '115211885171_10207363001455865_2905850678855681497_n.jpg', '2016-07-24', 2);
+INSERT INTO `estudiante` (`id`, `nombre`, `edad`, `curso`) VALUES
+(1, 'asd', 1, 2),
+(2, 'asdasd', 123, 123),
+(3, 'lala', 23, 20),
+(4, 'lalalalala', 123123, 123123);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `m_asientoscontables`
+--
+
+CREATE TABLE `m_asientoscontables` (
+  `cod_asiento` int(11) NOT NULL,
+  `num_asiento` int(11) NOT NULL,
+  `fecha_asiento` datetime NOT NULL,
+  `concepto` varchar(150) NOT NULL,
+  `cod_factura` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -106,7 +115,8 @@ INSERT INTO `m_costosfijos` (`cod_costosfijos`, `alquiler`, `luz`, `agua`, `herr
 (8, 2600, 500, 250, 200, '2016-08-20', 3),
 (10, 1500, 120, 90, 200, '2016-08-21', 8),
 (11, 1500, 120, 90, 200, '2016-10-18', 3),
-(12, 1500, 120, 90, 200, '2016-10-18', 3);
+(12, 1500, 120, 90, 200, '2016-10-18', 3),
+(13, 1500, 120, 90, 200, '2016-10-20', 3);
 
 -- --------------------------------------------------------
 
@@ -125,6 +135,20 @@ CREATE TABLE `m_costosgeneral` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `m_detalleasiento`
+--
+
+CREATE TABLE `m_detalleasiento` (
+  `cod_detalleasiento` int(11) NOT NULL,
+  `num_asiento` int(11) NOT NULL,
+  `cod_cuenta` varchar(150) NOT NULL,
+  `debe` double DEFAULT '0',
+  `haber` double DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `m_detalle_factura`
 --
 
@@ -136,28 +160,6 @@ CREATE TABLE `m_detalle_factura` (
   `cantidad` int(11) NOT NULL,
   `precio_venta` float NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `m_detalle_factura`
---
-
-INSERT INTO `m_detalle_factura` (`cod_detalle`, `cod_factura`, `num_factura`, `cod_producto`, `cantidad`, `precio_venta`) VALUES
-(71, 67, 20, 1, 3, 1822),
-(79, 74, 32, 4, 2, 1200),
-(78, 73, 21, 1, 2, 1822),
-(73, 68, 29, 3, 10, 2920),
-(77, 72, 20, 6, 7, 800),
-(70, 66, 19, 15, 8, 600),
-(69, 65, 28, 15, 14, 600),
-(76, 71, 19, 3, 2, 2920),
-(68, 65, 28, 4, 5, 1200),
-(75, 70, 31, 3, 2, 2920),
-(74, 69, 30, 1, 2, 1822),
-(55, 59, 17, 15, 2, 600),
-(60, 62, 17, 15, 3, 600),
-(41, 56, 25, 7, 3, 920),
-(72, 68, 29, 1, 5, 1822),
-(67, 65, 28, 3, 5, 2920);
 
 -- --------------------------------------------------------
 
@@ -173,85 +175,6 @@ CREATE TABLE `m_facturas` (
   `cod_clientes` int(11) DEFAULT NULL,
   `finalizado` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `m_facturas`
---
-
-INSERT INTO `m_facturas` (`cod_factura`, `tipo_factura`, `num_factura`, `fecha_factura`, `cod_clientes`, `finalizado`) VALUES
-(1, 'A', 1, '2016-10-16 04:29:41', 14, 0),
-(4, 'B', 1, '2016-10-16 04:35:12', 7, 0),
-(3, 'A', 2, '2016-10-16 04:32:02', 14, 0),
-(5, 'B', 2, '2016-10-16 04:35:31', 6, 0),
-(6, 'A', 3, '2016-10-16 04:35:44', 14, 0),
-(7, 'C', 1, '2016-10-16 04:38:36', NULL, 0),
-(8, 'C', 2, '2016-10-16 04:38:39', NULL, 0),
-(9, 'C', 3, '2016-10-16 04:38:52', NULL, 0),
-(10, 'A', 4, '2016-10-16 04:39:05', 14, 0),
-(11, 'B', 3, '2016-10-16 04:49:45', 6, 0),
-(12, 'C', 4, '2016-10-16 04:51:42', NULL, 0),
-(13, 'C', 5, '2016-10-16 05:01:44', NULL, 0),
-(14, 'A', 5, '2016-10-16 05:01:48', 14, 0),
-(15, 'C', 6, '2016-10-16 05:02:17', NULL, 0),
-(16, 'A', 6, '2016-10-16 05:02:27', 14, 0),
-(17, 'A', 7, '2016-10-16 05:03:09', 14, 0),
-(18, 'A', 8, '2016-10-16 05:08:43', 14, 0),
-(19, 'A', 9, '2016-10-16 05:13:13', 14, 0),
-(20, 'A', 10, '2016-10-16 05:30:33', 14, 0),
-(21, 'B', 4, '2016-10-16 17:42:55', 11, 0),
-(22, 'B', 5, '2016-10-16 17:48:51', 6, 0),
-(23, 'A', 11, '2016-10-16 17:48:55', 14, 0),
-(24, 'A', 12, '2016-10-16 17:49:02', 14, 0),
-(25, 'A', 13, '2016-10-16 17:53:06', 14, 0),
-(26, 'A', 14, '2016-10-16 18:04:49', 14, 0),
-(27, 'B', 6, '2016-10-16 18:27:18', 6, 0),
-(28, 'A', 15, '2016-10-16 18:33:58', 14, 0),
-(29, 'B', 7, '2016-10-16 18:35:49', 6, 0),
-(30, 'A', 16, '2016-10-16 18:49:12', 14, 0),
-(31, 'B', 8, '2016-10-16 18:52:20', 6, 0),
-(32, 'B', 9, '2016-10-16 18:52:25', 6, 0),
-(33, 'A', 17, '2016-10-16 19:00:25', 14, 0),
-(34, 'C', 7, '2016-10-16 19:00:43', NULL, 0),
-(35, 'A', 18, '2016-10-16 19:00:47', 14, 0),
-(36, 'B', 10, '2016-10-16 19:02:07', 6, 0),
-(37, 'A', 19, '2016-10-16 19:35:17', 14, 0),
-(38, 'B', 11, '2016-10-16 21:01:29', 7, 0),
-(39, 'B', 12, '2016-10-16 21:06:04', 11, 0),
-(40, 'A', 20, '2016-10-16 21:30:34', 14, 0),
-(41, 'A', 21, '2016-10-16 21:58:42', 14, 0),
-(42, 'A', 22, '2016-10-16 22:03:08', 14, 0),
-(43, 'B', 13, '2016-10-16 22:37:00', 6, 0),
-(44, 'C', 8, '2016-10-17 01:46:12', NULL, 0),
-(45, 'C', 9, '2016-10-17 01:47:27', NULL, 0),
-(46, 'C', 10, '2016-10-17 01:52:12', NULL, 0),
-(47, 'C', 11, '2016-10-17 01:56:00', NULL, 0),
-(48, 'C', 12, '2016-10-17 02:00:38', NULL, 0),
-(49, 'C', 13, '2016-10-18 13:48:42', NULL, 0),
-(50, 'A', 23, '2016-10-18 14:19:09', 14, 0),
-(51, 'B', 14, '2016-10-18 14:29:26', 11, 0),
-(52, 'A', 24, '2016-10-18 14:32:50', 14, 0),
-(53, 'C', 14, '2016-10-18 14:41:49', NULL, 0),
-(54, 'C', 15, '2016-10-18 14:44:34', NULL, 0),
-(55, 'C', 16, '2016-10-18 16:33:34', NULL, 0),
-(56, 'A', 25, '2016-10-18 16:52:19', 14, 0),
-(57, 'A', 26, '2016-10-18 18:36:59', 7, 0),
-(58, 'B', 15, '2016-10-18 20:17:07', 6, 0),
-(59, 'C', 17, '2016-10-18 20:17:39', NULL, 0),
-(60, 'B', 16, '2016-10-18 21:22:14', 6, 0),
-(61, 'A', 27, '2016-10-18 21:22:27', 7, 0),
-(62, 'B', 17, '2016-10-18 22:30:05', 6, 0),
-(63, 'B', 18, '2016-10-18 22:47:06', 6, 0),
-(64, 'C', 18, '2016-10-18 22:51:41', NULL, 0),
-(65, 'A', 28, '2016-10-18 23:46:03', 14, 1),
-(66, 'C', 19, '2016-10-19 00:24:44', NULL, 0),
-(67, 'C', 20, '2016-10-19 01:00:21', NULL, 0),
-(68, 'A', 29, '2016-10-19 01:00:58', 7, 0),
-(69, 'A', 30, '2016-10-19 01:04:37', 7, 0),
-(70, 'A', 31, '2016-10-19 01:05:54', 7, 1),
-(71, 'B', 19, '2016-10-19 01:06:46', 6, 1),
-(72, 'B', 20, '2016-10-19 01:09:04', 11, 1),
-(73, 'B', 21, '2016-10-19 14:12:15', 6, 1),
-(74, 'A', 32, '2016-10-19 14:21:10', 14, 1);
 
 -- --------------------------------------------------------
 
@@ -277,27 +200,6 @@ INSERT INTO `m_materiales` (`cod_material`, `descripcion`, `cod_unidad`, `precio
 (1, 'Pintura', 2, 250, -93, '2016-10-19 14:16:43', 7),
 (2, 'Alfombra Punzonada', 4, 120, 6, '2016-10-18 15:47:46', 7),
 (3, 'Barras de Silicona', 1, 2.5, -33, '2016-10-18 15:48:04', 5);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `m_mercaderia`
---
-
-CREATE TABLE `m_mercaderia` (
-  `cod_mercaderia` int(11) NOT NULL,
-  `descripcion` text NOT NULL,
-  `stock` int(11) NOT NULL,
-  `precio` float NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `m_mercaderia`
---
-
-INSERT INTO `m_mercaderia` (`cod_mercaderia`, `descripcion`, `stock`, `precio`) VALUES
-(1, 'Palet palet', 5, 200),
-(4, 'Rascador de Gatos', 89, 250);
 
 -- --------------------------------------------------------
 
@@ -381,7 +283,62 @@ INSERT INTO `m_operaciones` (`cod_operacion`, `cod_producto`, `cod_material`, `q
 (59, 15, NULL, 20, 1, NULL, '2016-10-19 01:09:43'),
 (60, 1, NULL, 2, 2, 73, '2016-10-19 14:12:29'),
 (61, NULL, 1, 2, 1, NULL, '2016-10-19 14:16:43'),
-(62, 4, NULL, 2, 2, 74, '2016-10-19 14:21:23');
+(62, 4, NULL, 2, 2, 74, '2016-10-19 14:21:23'),
+(63, 1, NULL, 8, 2, 75, '2016-10-19 16:06:23'),
+(64, 1, NULL, 8, 2, 80, '2016-10-20 08:18:06');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `m_plandecuentas`
+--
+
+CREATE TABLE `m_plandecuentas` (
+  `cod_cuenta` varchar(11) NOT NULL,
+  `detalle` varchar(150) NOT NULL,
+  `tipo` varchar(150) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `m_plandecuentas`
+--
+
+INSERT INTO `m_plandecuentas` (`cod_cuenta`, `detalle`, `tipo`) VALUES
+('1', 'Caja y Bancos', 'Activo'),
+('1.1.1', 'Caja', 'Activo'),
+('1.1.2 ', 'Banco', 'Activo'),
+('1.1.3', 'Valores a Depositar', 'Activo'),
+('1.2 ', 'Creditos', 'Activo'),
+('1.2.1', 'Deudores por Venta', 'Activo'),
+('1.2.2', 'Documentos a Cobrar', 'Activo'),
+('1.3', 'Bienes de Cambio', 'Activo'),
+('1.3.1', 'Mercaderias', 'Activo'),
+('1.3.2', 'Materias Primas', 'Activo'),
+('1.4', 'Bienes de Uso', 'Activo'),
+('1.4.1', 'Muebles y Utiles', 'Activo'),
+('1.4.2', 'Instalaciones', 'Activo'),
+('1.4.3', 'Rodados', 'Activo'),
+('1.4.4', 'Inmuebles', 'Activo'),
+('1.4.5', 'Terrenos', 'Activo'),
+('1.4.6', 'Equipos de Computacion', 'Activo'),
+('2.1', 'Deudas', 'Pasivo'),
+('2.1.1', 'Proveedores', 'Pasivo'),
+('2.1.2', 'Acreedores Varios', 'Pasivo'),
+('2.1.3', 'Documentos a Pagar', 'Pasivo'),
+('3.1', 'Capital', 'Patrimonio Neto'),
+('3.2', 'Reserva Legal', 'Patrimonio Neto'),
+('3.3', 'Resultado del Ejercicio', 'Patrimonio Neto'),
+('4.1', 'Costo de Mercaderias Vendidas', 'Gastos'),
+('4.2', 'Gastos Generales', 'Gastos'),
+('4.3', 'Alquileres Pagados', 'Gastos'),
+('4.4', 'Sueldos y Jornales', 'Gastos'),
+('4.5', 'Seguros', 'Gastos'),
+('4.6', 'Fletes y Acarreos', 'Gastos'),
+('4.7', 'Gastos de Libreria', 'Gastos'),
+('5.1', 'Ventas', 'Ingresos'),
+('5.2', 'Alquileres Cobrados', 'Ingresos'),
+('5.3', 'Seguros Cobrados', 'Ingresos'),
+('0', '', '');
 
 -- --------------------------------------------------------
 
@@ -404,7 +361,7 @@ CREATE TABLE `m_productos` (
 --
 
 INSERT INTO `m_productos` (`cod_producto`, `descripcion`, `stock`, `precio_sugerido`, `precio_unitario`, `fecha_alta`, `fecha_stock`) VALUES
-(1, 'Tower Cat V.1', 71, NULL, 1822, NULL, '2016-10-19 14:12:29'),
+(1, 'Tower Cat V.1', 55, NULL, 1822, NULL, '2016-10-20 08:18:06'),
 (3, 'Tower Cat V.5', 64, 2826.9, 2920, '2016-10-14', '2016-10-19 01:06:54'),
 (4, 'Arenero Paw Paw xd', 88, 1067.8, 1200, '2016-10-15', '2016-10-19 14:21:23'),
 (6, 'Rascador de Gatos', 40, 733.55, 800, '2016-10-16', '2016-10-19 01:09:21'),
@@ -485,25 +442,6 @@ INSERT INTO `m_unidadesmedida` (`cod_unidad`, `nombre_unidad`, `abreviatura_unid
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `secciones`
---
-
-CREATE TABLE `secciones` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(80) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `secciones`
---
-
-INSERT INTO `secciones` (`id`, `nombre`) VALUES
-(1, 'wea'),
-(2, 'wea2');
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `t_costostemp`
 --
 
@@ -565,49 +503,22 @@ INSERT INTO `t_costosvariables` (`cod_costosvariables`, `cod_producto`, `mano_ob
 (10, 13, 20, 132, '2016-10-18'),
 (11, 15, 20, 600, '2016-10-18');
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `t_facturatemp`
---
-
-CREATE TABLE `t_facturatemp` (
-  `cod_tmp` int(11) NOT NULL,
-  `cod_producto` int(11) NOT NULL,
-  `cantidad_tmp` int(11) NOT NULL,
-  `precio_tmp` double DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `t_tipofactura`
---
-
-CREATE TABLE `t_tipofactura` (
-  `cod_tipofactura` int(11) NOT NULL,
-  `tipofactura` varchar(200) NOT NULL,
-  `detalles` text NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `t_tipofactura`
---
-
-INSERT INTO `t_tipofactura` (`cod_tipofactura`, `tipofactura`, `detalles`) VALUES
-(1, 'Factura A', 'Si se trata de operaciones entre responsables inscriptos, existen 3 tipos de comprobantes que se pueden utilizar:\r\nTipo “A”\r\nTipo “A” con leyenda “pago en CBU informada”\r\nTipo “M”'),
-(2, 'Factura B', 'Cuando se trate de una operación entre un Responsable Inscripto y Monotributista, Consumidor Final, o Exento, el Responsable Inscripto deberá emitir comprobantes tipo “B”.'),
-(3, 'Factura C', 'En caso de que quien lo emita no sea un Responsable Inscripto (Monotributista; exento en el IVA), deberá operar con comprobantes tipo “C”.');
-
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `estudiantes`
+-- Indices de la tabla `estudiante`
 --
-ALTER TABLE `estudiantes`
+ALTER TABLE `estudiante`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `m_asientoscontables`
+--
+ALTER TABLE `m_asientoscontables`
+  ADD PRIMARY KEY (`cod_asiento`),
+  ADD UNIQUE KEY `num_asiento` (`num_asiento`);
 
 --
 -- Indices de la tabla `m_clientes`
@@ -629,6 +540,12 @@ ALTER TABLE `m_costosgeneral`
   ADD PRIMARY KEY (`cod_costos`);
 
 --
+-- Indices de la tabla `m_detalleasiento`
+--
+ALTER TABLE `m_detalleasiento`
+  ADD PRIMARY KEY (`cod_detalleasiento`);
+
+--
 -- Indices de la tabla `m_detalle_factura`
 --
 ALTER TABLE `m_detalle_factura`
@@ -647,16 +564,16 @@ ALTER TABLE `m_materiales`
   ADD PRIMARY KEY (`cod_material`);
 
 --
--- Indices de la tabla `m_mercaderia`
---
-ALTER TABLE `m_mercaderia`
-  ADD PRIMARY KEY (`cod_mercaderia`);
-
---
 -- Indices de la tabla `m_operaciones`
 --
 ALTER TABLE `m_operaciones`
   ADD PRIMARY KEY (`cod_operacion`);
+
+--
+-- Indices de la tabla `m_plandecuentas`
+--
+ALTER TABLE `m_plandecuentas`
+  ADD UNIQUE KEY `cod_cuenta` (`cod_cuenta`);
 
 --
 -- Indices de la tabla `m_productos`
@@ -684,38 +601,25 @@ ALTER TABLE `m_unidadesmedida`
   ADD PRIMARY KEY (`cod_unidad`);
 
 --
--- Indices de la tabla `secciones`
---
-ALTER TABLE `secciones`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indices de la tabla `t_costosvariables`
 --
 ALTER TABLE `t_costosvariables`
   ADD PRIMARY KEY (`cod_costosvariables`);
 
 --
--- Indices de la tabla `t_facturatemp`
---
-ALTER TABLE `t_facturatemp`
-  ADD PRIMARY KEY (`cod_tmp`);
-
---
--- Indices de la tabla `t_tipofactura`
---
-ALTER TABLE `t_tipofactura`
-  ADD PRIMARY KEY (`cod_tipofactura`);
-
---
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `estudiantes`
+-- AUTO_INCREMENT de la tabla `estudiante`
 --
-ALTER TABLE `estudiantes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+ALTER TABLE `estudiante`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT de la tabla `m_asientoscontables`
+--
+ALTER TABLE `m_asientoscontables`
+  MODIFY `cod_asiento` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `m_clientes`
 --
@@ -725,37 +629,37 @@ ALTER TABLE `m_clientes`
 -- AUTO_INCREMENT de la tabla `m_costosfijos`
 --
 ALTER TABLE `m_costosfijos`
-  MODIFY `cod_costosfijos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `cod_costosfijos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT de la tabla `m_costosgeneral`
 --
 ALTER TABLE `m_costosgeneral`
   MODIFY `cod_costos` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT de la tabla `m_detalleasiento`
+--
+ALTER TABLE `m_detalleasiento`
+  MODIFY `cod_detalleasiento` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT de la tabla `m_detalle_factura`
 --
 ALTER TABLE `m_detalle_factura`
-  MODIFY `cod_detalle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
+  MODIFY `cod_detalle` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `m_facturas`
 --
 ALTER TABLE `m_facturas`
-  MODIFY `cod_factura` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
+  MODIFY `cod_factura` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `m_materiales`
 --
 ALTER TABLE `m_materiales`
   MODIFY `cod_material` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
--- AUTO_INCREMENT de la tabla `m_mercaderia`
---
-ALTER TABLE `m_mercaderia`
-  MODIFY `cod_mercaderia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
 -- AUTO_INCREMENT de la tabla `m_operaciones`
 --
 ALTER TABLE `m_operaciones`
-  MODIFY `cod_operacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+  MODIFY `cod_operacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
 --
 -- AUTO_INCREMENT de la tabla `m_productos`
 --
@@ -777,25 +681,10 @@ ALTER TABLE `m_tipo_operacion`
 ALTER TABLE `m_unidadesmedida`
   MODIFY `cod_unidad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
--- AUTO_INCREMENT de la tabla `secciones`
---
-ALTER TABLE `secciones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
 -- AUTO_INCREMENT de la tabla `t_costosvariables`
 --
 ALTER TABLE `t_costosvariables`
   MODIFY `cod_costosvariables` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
---
--- AUTO_INCREMENT de la tabla `t_facturatemp`
---
-ALTER TABLE `t_facturatemp`
-  MODIFY `cod_tmp` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `t_tipofactura`
---
-ALTER TABLE `t_tipofactura`
-  MODIFY `cod_tipofactura` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
